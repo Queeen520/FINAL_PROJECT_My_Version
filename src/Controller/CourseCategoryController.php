8cba726449597736053171bf205631fd3238964f
k4144 <kemper144@gmail.com>
2022-12-07 14:00:33 +0100

    ...

--- src/Controller/CourseCategoryController.php
+++ src/Controller/CourseCategoryController.php
@@ -78,6 +78,8 @@ class CourseCategoryController extends AbstractController
             
             //pictureUrl is the name given to the input field
             if ($pictureFile) {
+                // check if existing image is not null and file exists
+                if($imageGallery->getImage() && file_exists('pictures/'.$imageGallery->getImage())){
                 unlink('pictures/'.$courseCategory->getImage());
                 $pictureFileName = $fileUploader->upload($pictureFile);
                 $courseCategory->setImage($pictureFileName);
