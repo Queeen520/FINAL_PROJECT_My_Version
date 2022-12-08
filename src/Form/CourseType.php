168a9a18dc4b08f87379b94b9208f4088cb4812d
Isabella W <isabellawindhaber@gmail.com>
2022-12-08 11:39:43 +0100

    ...

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -8,6 +8,8 @@ use App\Entity\Price;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 
 // used for foreign key 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -18,8 +20,8 @@ class CourseType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startTime')
-            ->add('endTime')
+            ->add('startTime', DateTimeType::class, ["attr"=>["class"=>"form-control mb-2"]])
+            ->add('endTime', DateTimeType::class, ["attr"=>["class"=>"form-control mb-2"]])
             // foreign key Category
             ->add('fkCourseCategory', EntityType::class,[
                 "attr"=>["placeholder"=>"please type Category name", "class"=>"form-control mb-2"],
