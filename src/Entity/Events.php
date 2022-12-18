7f2b1c0bfc1f9c2b50dbfc4787ac3441cf10251d
Isabella W <isabellawindhaber@gmail.com>
2022-12-18 18:23:30 +0100

    ...

---
+++ src/Entity/Events.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventsRepository::class)]
+class Events
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $beginAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $endAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titel = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Priority = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBeginAt(): ?\DateTimeInterface
+    {
+        return $this->beginAt;
+    }
+
+    public function setBeginAt(\DateTimeInterface $beginAt): self
+    {
+        $this->beginAt = $beginAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeInterface
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(?\DateTimeInterface $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getTitel(): ?string
+    {
+        return $this->Titel;
+    }
+
+    public function setTitel(string $Titel): self
+    {
+        $this->Titel = $Titel;
+
+        return $this;
+    }
+
+    public function getPriority(): ?string
+    {
+        return $this->Priority;
+    }
+
+    public function setPriority(?string $Priority): self
+    {
+        $this->Priority = $Priority;
+
+        return $this;
+    }
+}
